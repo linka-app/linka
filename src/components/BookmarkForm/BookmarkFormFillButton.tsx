8bb73a937db696cbc80a45cbe25f9af4844f456f
gregory-lifhits-assaabloy <gregory.lifhits@assaabloy.com>
2023-04-27 07:34:11 -0400

    🌐 i18n(bookmarkFormFillButton.tsx): add translation support for BookmarkFormFillButton component ✨ feat(bookmarkFormFillButton.tsx): add translation support for BookmarkFormFillButton component The translation support for the BookmarkFormFillButton component has been added to the i18n files. The component now uses the translation for the button text and the error message when a URL is not provided.

--- src/components/BookmarkForm/BookmarkFormFillButton.tsx
+++ src/components/BookmarkForm/BookmarkFormFillButton.tsx
@@ -1,6 +1,7 @@
 import { browserlessDoScrape } from '@/api/browserless';
 import { doDescArticle } from '@/api/openai';
 import { useContexts } from '@/hooks';
+import { I18nLocals, i18n } from '@/i18n';
 import { getConfig } from '@/utils/getConfig/getConfig';
 import PsychologySharpIcon from '@mui/icons-material/PsychologySharp';
 import { LoadingButton } from '@mui/lab';
@@ -9,49 +10,64 @@ import React, { useState } from 'react';
 import { useFormContext } from 'react-hook-form-mui'; // instead of react-hook-form
 
 export const BookmarkFormFillButton: React.FC = () => {
+  const config = getConfig();
+  const translation = i18n[(config?.language as I18nLocals) || 'en'];
+
   const { watch, setValue } = useFormContext();
   const theUrl = watch('url', false);
   const { doToast, doLoading } = useContexts();
   const [loading, setLoading] = useState<boolean>(false);
 
-  const config = getConfig();
-
   const doFill = () => {
     setLoading(true);
     doLoading(true);
     if (theUrl != null) {
-      browserlessDoScrape({ url: theUrl }).then((res: string) => {
-        if (res.length > 0) {
-          doDescArticle({ message: res })
-            .then((res: { title: string; desc: string; tags: string[] }) => {
-              setValue('title', res.title);
-              setValue('description', res.desc);
-              setValue(
-                'tag_names',
-                res.tags.map((tag: any) => {
-                  return tag.replaceAll(' ', '-').toLowerCase();
-                })
-              );
-              setLoading(false);
-              doLoading(false);
-            })
-            .catch((err: any) => {
-              console.log(err);
-              doToast({
-                open: true,
-                type: 'error',
-                title: 'Failed',
+      browserlessDoScrape({ url: theUrl })
+        .then((res: string) => {
+          if (res.length > 0) {
+            doDescArticle({ message: res })
+              .then((res: { title: string; desc: string; tags: string[] }) => {
+                setValue('title', res.title);
+                setValue('description', res.desc);
+                setValue(
+                  'tag_names',
+                  res.tags.map((tag: any) => {
+                    return tag.replaceAll(' ', '-').toLowerCase();
+                  })
+                );
+                setLoading(false);
+                doLoading(false);
+              })
+              .catch((err: any) => {
+                console.log(err);
+                doToast({
+                  open: true,
+                  type: 'error',
+                  title: translation.statusFailed,
+                  description: err,
+                });
+                setLoading(false);
+                doLoading(false);
               });
-              setLoading(false);
-              doLoading(false);
-            });
-        }
-      });
+          }
+        })
+        .catch((err: any) => {
+          console.log(err);
+          doToast({
+            open: true,
+            type: 'error',
+            title: translation.statusFailed,
+            description: err,
+          });
+          setLoading(false);
+          doLoading(false);
+        });
     } else {
       doToast({
         open: true,
         type: 'error',
-        title: 'A URL is required.',
+        title: translation.statusFailed,
+        description: translation.bookmarkFormFillButtonURLRequired,
       });
       setLoading(false);
       doLoading(false);
@@ -69,7 +85,7 @@ export const BookmarkFormFillButton: React.FC = () => {
       onClick={doFill}
       fullWidth
     >
-      Fill with Chat GPT
+      {translation.bookmarkFormFillButtonFillWithChatGPT}
     </LoadingButton>
   );
 };
