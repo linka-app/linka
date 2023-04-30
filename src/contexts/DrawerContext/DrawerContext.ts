fb45bab93aa3c26e5be9f3f13d6a7ec991de9311
Greg L <sincore@gmail.com>
2023-04-30 10:43:09 -0400

    Refract: Removing unused context

--- src/contexts/DrawerContext/DrawerContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-import { DrawerContextType } from './DrawerContextProps';
-
-export const DrawerContext = createContext<DrawerContextType | null>(null);
-export default DrawerContext;
